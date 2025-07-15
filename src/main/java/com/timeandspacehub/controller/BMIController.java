305b8c191c1fcabda4a45f33988e48ff197c6860
Kaushal Silwal <silwalkaushal1001@gmail.com>
2025-07-15 10:50:27 -0400

    TB-12 : Add UK metrics for BMI App

--- src/main/java/com/timeandspacehub/controller/BMIController.java
+++ src/main/java/com/timeandspacehub/controller/BMIController.java
@@ -1,12 +1,17 @@
 package com.timeandspacehub.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.timeandspacehub.service.BMIService;
+
 @RestController
 public class BMIController {
-
+	@Autowired
+	private BMIService bmiService;
 	@GetMapping("/bmi")
 	public String hello() {
 		return "Welcome to BMI APP!";
@@ -34,6 +39,17 @@ public class BMIController {
 		
 		return String.format("Your BMI is %.2f - %s", bmi, category);
 		
+	}
+	@GetMapping("/calculate")
+	public ResponseEntity<String> getMetrics(@RequestParam String unitSystem, @RequestParam double meterInput, @RequestParam double kilogramInput ) {
+		double bmi;
+		if ("UK".equalsIgnoreCase(unitSystem)) {
+			bmi = bmiService.getMetrics(kilogramInput,meterInput);
+		} else {
+			return ResponseEntity.badRequest().body("Invalid unit system. Use 'UK' or 'US'");
+		}
+		return ResponseEntity.ok("Your BMI is: " + bmi + "(" + bmiService.getCategory(bmi) + ")");
+
 	}
 
 
